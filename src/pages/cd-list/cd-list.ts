29d2805b954e5725dd0e7799a15d24c32549e7bc
Arouna31 <arounakinda@gmail.com>
2019-01-10 21:33:52 +0000

    connect CdListPage with ObjectsService

--- src/pages/cd-list/cd-list.ts
+++ src/pages/cd-list/cd-list.ts
@@ -1,22 +1,39 @@
 import { Component } from "@angular/core";
-import { NavController, NavParams, MenuController } from "ionic-angular";
+import {
+  NavController,
+  NavParams,
+  MenuController,
+  ModalController
+} from "ionic-angular";
+import { LendCdPage } from "../lend-cd/lend-cd";
+import { Cd } from "../../models/cd.model";
+import { ObjectsService } from "../../services/Objects.service";
 
 @Component({
   selector: "page-cd-list",
   templateUrl: "cd-list.html"
 })
 export class CdListPage {
+  CdList: Cd[] = [];
+
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private menuCtrl: MenuController
+    private menuCtrl: MenuController,
+    private objectsService: ObjectsService,
+    private modalCtrl: ModalController
   ) {}
 
-  ionViewDidLoad() {
-    console.log("ionViewDidLoad CdListPage");
+  ionViewWillEnter() {
+    this.CdList = this.objectsService.cd;
   }
 
   onToggleMenu() {
     this.menuCtrl.open();
+  }
+
+  onSelectedCd(id: number) {
+    const cdModal = this.modalCtrl.create(LendCdPage, { index: id });
+    cdModal.present();
   }
 }
--- src/pages/lend-cd/lend-cd.ts
+++ src/pages/lend-cd/lend-cd.ts
@@ -1,24 +1,32 @@
-import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-
-/**
- * Generated class for the LendCdPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { Component } from "@angular/core";
+import { NavController, NavParams, ViewController } from "ionic-angular";
+import { Cd } from "../../models/cd.model";
+import { ObjectsService } from "../../services/Objects.service";
 
 @Component({
-  selector: 'page-lend-cd',
-  templateUrl: 'lend-cd.html',
+  selector: "page-lend-cd",
+  templateUrl: "lend-cd.html"
 })
 export class LendCdPage {
+  lenCdPage: any = LendCdPage;
+  cd: Cd;
+  index: number;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(
+    public navCtrl: NavController,
+    public navParams: NavParams,
+    private objectsService: ObjectsService,
+    private viewCtrl: ViewController
+  ) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.index = this.navParams.get("index");
+    this.cd = this.objectsService.cd[this.index];
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LendCdPage');
+  onDismissModal() {
+    this.viewCtrl.dismiss();
   }
-
 }
